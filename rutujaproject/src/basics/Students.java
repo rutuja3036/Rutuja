c7a7e576f2dc9c9a4ea4f25c1e086da9ca815128
prathamesh <prathamesh@LAPTOP-N400V4R0>
2021-10-22 19:40:06 +0530

    Methods in java

---
+++ rutujaproject/src/basics/Students.java
@@ -0,0 +1,22 @@
+package basics;
+
+public class Students {
+	int roll;
+	String name;
+	public int getRoll() 
+	{
+		return roll;
+		}
+	public void setRoll(int roll)
+	{
+		this.roll=roll;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+	this.name=name;
+}
+}
