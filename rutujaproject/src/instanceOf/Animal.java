6c0533f56437e92bb64efb3786754eba3cb7f767
prathamesh <prathamesh@LAPTOP-N400V4R0>
2021-10-25 19:43:21 +0530

    instanceof in java

---
+++ rutujaproject/src/instanceOf/Animal.java
@@ -0,0 +1,10 @@
+package instanceOf;
+
+class Animal{}  
+class Dog1 extends Animal{//Dog inherits Animal  
+  
+ public static void main(String args[]){  
+ Dog1 d=new Dog1();  
+ System.out.println(d instanceof Animal);//true  
+ }  
+}  
